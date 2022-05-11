bd400108ff5611ea895266c6406afefba376e5b1
vdornadula <vaishnavidornadula2026@u.northwestern.edu>
2022-05-10 20:06:10 -0500

    summing sin functions

--- Homework10/code.py
+++ Homework10/code.py
@@ -1,1 +1,12 @@
+import board
+import time
+import digitalio
+import analogio
+from ulab import numpy as np # to get access to ulab numpy functions
+
+sum_sin = np.zeros((1024,1))
+
+for i in range(1024):
+    sum_sin[i] = np.sin(i)+np.sin(6*i)+np.sin(4*i)
+
 print("Hello World!")
